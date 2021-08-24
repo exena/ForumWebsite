bcda46dfd566c4a55e1a169817ed181c816a3ffa
exena <exena@naver.com>
2021-08-24 15:04:52 +0900

    init

---
+++ src/main/java/gameinfoweb/gameinfo/GameinfoApplication.java
@@ -0,0 +1,13 @@
+package gameinfoweb.gameinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gameinfoweb/gameinfo/GameinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package gameinfoweb.gameinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
