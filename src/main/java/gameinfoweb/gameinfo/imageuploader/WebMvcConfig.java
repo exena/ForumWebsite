63f8b0750637fe543ef9eae57d3630420f5bbd19
exena <exena@naver.com>
2021-09-23 19:19:59 +0900

    summernote image upload

--- src/main/java/gameinfoweb/gameinfo/imageuploader/WebMvcConfig.java
+++ src/main/java/gameinfoweb/gameinfo/imageuploader/WebMvcConfig.java
@@ -13,6 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/summernoteImage/**")
-                .addResourceLocations("file:///src/main/resources/static/summernote_image/");
+                .addResourceLocations("file:///home/osusml2135/gameinfo/src/main/resources/static/summernote_image/");
     }
 }
