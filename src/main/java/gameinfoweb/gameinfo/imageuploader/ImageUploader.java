cef4824b1eb8b97bac6c3126683ac8f8cb0d59d3
exena <exena@naver.com>
2021-09-23 17:33:25 +0900

    summernote image upload

--- src/main/java/gameinfoweb/gameinfo/imageuploader/ImageUploader.java
+++ src/main/java/gameinfoweb/gameinfo/imageuploader/ImageUploader.java
@@ -21,7 +21,7 @@ public class ImageUploader {
 
         JsonObject jsonObject = new JsonObject();
 
-        String fileRoot = "summernote_image\\";	//저장될 외부 파일 경로
+        String fileRoot = "home\\osusml2135\\summernote_image\\";	//저장될 외부 파일 경로
         String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
         String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
 
